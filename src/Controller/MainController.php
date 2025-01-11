4bfd2044ca6c14b4e4f886eb907ab059bce9cc07
EdouardPlantevin <plantevin.contact@gmail.com>
2025-01-11 21:46:05 +0100

    php-cs-fixer

---
+++ src/Controller/MainController.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Attribute\Route;
+
+class MainController extends AbstractController
+{
+    #[Route('/')]
+    public function homepage(): Response
+    {
+        return new Response('<h1>Hello World !</h1>');
+    }
+}
