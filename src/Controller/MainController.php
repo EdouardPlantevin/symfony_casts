6ceba8bd38c3f164172dca83b4903e3a3f35e435
EdouardPlantevin <plantevin.contact@gmail.com>
2025-01-11 22:13:01 +0100

    add twig

--- src/Controller/MainController.php
+++ src/Controller/MainController.php
@@ -11,6 +11,19 @@ class MainController extends AbstractController
     #[Route('/')]
     public function homepage(): Response
     {
-        return new Response('<h1>Hello World !</h1>');
+
+        $startshipCount = 4;
+
+        $myShip = [
+            "name" => "USS LeafyCruiser (NCC-0001)",
+            "class" => "Garden",
+            "captain" => "Jean-Luc Pickels",
+            "status" => "under construction"
+        ];
+
+        return $this->render('main/homepage.html.twig', [
+            'numberOfStarships' => $startshipCount,
+            'myShip' => $myShip
+        ]);
     }
 }
