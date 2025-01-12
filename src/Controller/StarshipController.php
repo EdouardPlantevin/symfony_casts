1314b814fdde7548da4538e53fc4dba342c0fcd4
EdouardPlantevin <plantevin.contact@gmail.com>
2025-01-12 16:55:41 +0100

    show starship

---
+++ src/Controller/StarshipController.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace App\Controller;
+
+use App\Repository\StarshipRepository;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Attribute\Route;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+
+class StarshipController extends AbstractController
+{
+
+    #[Route('/starships/{id<\d+>}', name: 'app_starships_show')]
+    public function show(int $id, StarshipRepository $starshipRepository): Response
+    {
+        $ship = $starshipRepository->find($id); 
+        
+        if (!$ship) {
+            throw $this->createNotFoundException('Starship not found');
+        }
+
+        return $this->render('starship/show.html.twig', [
+            'ship' => $ship,
+        ]);
+    }
+}
